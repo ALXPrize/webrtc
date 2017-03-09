5ea5450917ab3a060272735fd7f2797d743592d6
GTL <cclark@gametheorylabs.com>
2017-03-09 16:10:29 -0600

    Added leftroom event for sending onLeave method

--- audio.js
+++ audio.js
@@ -1,5 +1,5 @@
-CodexPacket = function(e){
-    
+CodexPacket = function(e) {
+
     //
     //  Local Recv Types: onjoin, onclientjoin, onclose
     //
@@ -10,17 +10,17 @@ CodexPacket = function(e){
     this.receiver = "";
     this.parent = "";
 
-    if(e){
+    if (e) {
         var input = JSON.parse(e.data);
         this.type = input.type.toLowerCase();
         this.data = input.data;
     }
 }
-CodexPacket.prototype.Serialize = function(){
+CodexPacket.prototype.Serialize = function() {
     return JSON.stringify(this);
 }
-CodexPacket.prototype.Send = function(){
-    parent.postMessage(this.Serialize(),"*");
+CodexPacket.prototype.Send = function() {
+    parent.postMessage(this.Serialize(), "*");
 }
 
 // grab the room from the URL
@@ -44,10 +44,10 @@ function track(name, info) {
 
 function doJoin(room) {
     webrtc.startLocalVideo();
-    webrtc.createRoom(room, function (err, name) {
+    webrtc.createRoom(room, function(err, name) {
         var newUrl = (framed ? document.referrer : window.parent.location.pathname) + '?' + room;
         if (!err) {
-            if (!framed) window.parent.history.replaceState({foo: 'bar'}, null, newUrl);
+            if (!framed) window.parent.history.replaceState({ foo: 'bar' }, null, newUrl);
             setRoom(room);
         } else {
             console.log('error', err, room);
@@ -120,7 +120,7 @@ function Start() {
         };
     });
 
-    webrtc.on('readyToCall', function () {
+    webrtc.on('readyToCall', function() {
 
         var packet = new CodexPacket();
         packet.type = "onstart"
@@ -149,7 +149,7 @@ function Start() {
     });
 
     // called when a peer is created
-    webrtc.on('createdPeer', function (peer) {
+    webrtc.on('createdPeer', function(peer) {
         var remotes = document.getElementById('remotes');
         if (!remotes) return;
 
@@ -183,71 +183,70 @@ function Start() {
         mute.style.visibility = 'hidden';
         d.appendChild(mute);
 
-        if(gender == 0){
-            webrtc.sendToAll('avatar', {avatar: "img/female.png"});
-        }
-        else{
-            webrtc.sendToAll('avatar', {avatar: "img/male.png"});
+        if (gender == 0) {
+            webrtc.sendToAll('avatar', { avatar: "img/female.png" });
+        } else {
+            webrtc.sendToAll('avatar', { avatar: "img/male.png" });
         }
 
         mute.onclick = function() {
-          if (peer.videoEl.muted) { // unmute
-            mute.className = 'button button-small button-mute';
-          } else { // mute
-            mute.className = 'button button-small button-mute muted';
-          }
-          peer.videoEl.muted = !peer.videoEl.muted;
+            if (peer.videoEl.muted) { // unmute
+                mute.className = 'button button-small button-mute';
+            } else { // mute
+                mute.className = 'button button-small button-mute muted';
+            }
+            peer.videoEl.muted = !peer.videoEl.muted;
         };
 
         if (peer && peer.pc) {
             peer.firsttime = true;
-            peer.pc.on('iceConnectionStateChange', function (event) {
+            peer.pc.on('iceConnectionStateChange', function(event) {
                 var state = peer.pc.iceConnectionState;
                 container.className = 'peerContainer p2p' +
                     state.substr(0, 1).toUpperCase() +
                     state.substr(1);
                 switch (state) {
-                case 'connected':
-                case 'completed':
-                    var packet = new CodexPacket();
-                    packet.type = "onclientjoin"
-                    packet.data.message = "Client Connected";
-                    packet.receiver = parentSrc;
-                    packet.Send();
-                    //audio.srcObject = peer.stream;
-                    mute.style.visibility = 'visible';
-                    if (peer.firsttime) {
-                        peer.firsttime = false;
-                        track('iceSuccess', {
-                            session: peer.sid,
-                            peerprefix: peer.browserPrefix,
-                            prefix: webrtc.capabilities.prefix,
-                            version: webrtc.capabilities.browserVersion
-                        });
-                    }
-                    break;
-                case 'closed':
-                    container.remove();
-                    var packet = new CodexPacket();
-                    packet.type = "onclientleave"
-                    packet.data.message = "Client Left";
-                    packet.receiver = parentSrc;
-                    packet.Send();
-                    break;
+                    case 'connected':
+                    case 'completed':
+                        var packet = new CodexPacket();
+                        packet.type = "onclientjoin"
+                        packet.data.message = "Client Connected";
+                        packet.receiver = parentSrc;
+                        packet.Send();
+                        //audio.srcObject = peer.stream;
+                        mute.style.visibility = 'visible';
+                        if (peer.firsttime) {
+                            peer.firsttime = false;
+                            track('iceSuccess', {
+                                session: peer.sid,
+                                peerprefix: peer.browserPrefix,
+                                prefix: webrtc.capabilities.prefix,
+                                version: webrtc.capabilities.browserVersion
+                            });
+                        }
+                        break;
+                    case 'closed':
+                        container.remove();
+                        var packet = new CodexPacket();
+                        packet.type = "onclientleave"
+                        packet.data.message = "Client Left";
+                        packet.receiver = parentSrc;
+                        packet.Send();
+                        break;
                 }
             });
         }
         remotes.appendChild(container);
     });
 
-    webrtc.connection.on('message', function (message) {
+    webrtc.connection.on('message', function(message) {
         var peers = self.webrtc.getPeers(message.from, message.roomType);
         if (!peers && peers.length > 0) return;
         var peer = peers[0];
 
         // FIXME: also send current avatar and nick to newly joining participants
-        try{
-             var container = document.getElementById('container_' + webrtc.getDomId(peer));
+        try {
+            var container = document.getElementById('container_' + webrtc.getDomId(peer));
             if (message.type === 'nickname') {
                 container.querySelector('.nick').textContent = message.payload.nick;
             } else if (message.type === 'avatar') {
@@ -255,21 +254,20 @@ function Start() {
             } else if (message.type === 'offer') {
                 // update things
                 if (nick) {
-                    peer.send('nickname', {nick: nick});
+                    peer.send('nickname', { nick: nick });
                 }
                 if (avatar) {
-                    peer.send('avatar', {avatar: avatar});
+                    peer.send('avatar', { avatar: avatar });
                 }
             }
-        }
-        catch(e){
+        } catch (e) {
             console.log("Error on WebRTC message: " + e);
         }
-       
+
     });
 
     // local p2p/ice failure
-    webrtc.on('iceFailed', function (peer) {
+    webrtc.on('iceFailed', function(peer) {
         console.log('local fail', peer.sid);
         track('iceFailed', {
             source: 'local',
@@ -280,8 +278,16 @@ function Start() {
         });
     });
 
+    webrtc.on('leftRoom', function(roomName) {
+        var packet = new CodexPacket();
+        packet.type = "onleave"
+        packet.data.message = "Left Room " + roomName;
+        packet.receiver = pkg.sender;
+        packet.Send();
+    });
+
     // remote p2p/ice failure
-    webrtc.on('connectivityError', function (peer) {
+    webrtc.on('connectivityError', function(peer) {
         console.log('remote fail', peer.sid);
         track('iceFailed', {
             source: 'remote',
@@ -298,117 +304,103 @@ function Start() {
         document.querySelector('form#createRoom>button').disabled = true;
     } else if (navigator && navigator.mediaDevices && navigator.mediaDevices.enumerateDevices) {
         navigator.mediaDevices.enumerateDevices()
-        .then(function (devices) {
-            var cameras = devices.filter(function(device) { return device.kind === 'videoinput'; });
-            hasCameras = cameras.length;
-            var mics = devices.filter(function(device) { return device.kind === 'audioinput'; });
-            if (mics.length) {
-                //document.getElementById('requirements').style.display = 'none';
-                //if (queryGum) webrtc.startLocalVideo();
-                webrtc.startLocalVideo();
-            } 
-            // else {
-            //     document.getElementById('microphoneWarning').style.display = 'block';
-            //     document.querySelector('form#createRoom>button').disabled = true;
-            // }
-        });
+            .then(function(devices) {
+                var cameras = devices.filter(function(device) { return device.kind === 'videoinput'; });
+                hasCameras = cameras.length;
+                var mics = devices.filter(function(device) { return device.kind === 'audioinput'; });
+                if (mics.length) {
+                    //document.getElementById('requirements').style.display = 'none';
+                    //if (queryGum) webrtc.startLocalVideo();
+                    webrtc.startLocalVideo();
+                }
+                // else {
+                //     document.getElementById('microphoneWarning').style.display = 'block';
+                //     document.querySelector('form#createRoom>button').disabled = true;
+                // }
+            });
     }
 }
 
-var onMessage = function(e){
+var onMessage = function(e) {
     console.log("Message: " + e.data);
     var pkg = new CodexPacket(e);
-    
 
-    if(pkg.type == "start"){
+
+    if (pkg.type == "start") {
         room = pkg.data.room;
         parentSrc = pkg.sender;
         Start(pkg);
-    }
-    else if(pkg.type == "join"){
+    } else if (pkg.type == "join") {
         room = pkg.data.room;
-        webrtc.joinRoom(room, function (err, res) {
-                var packet = new CodexPacket();
-                if (err){
-                    packet.type = "error";
-                    packet.data.cmd = "onjoin";
-                    packet.data.value = room;
-                    packet.receiver = pkg.sender;
-                    packet.Send();
-                    return;
-                }
-                else{
-                    packet.type = "onjoin"
-                    packet.data.message = "Joined Room " + room;
-                    packet.receiver = pkg.sender;
-                    packet.Send();
+        webrtc.joinRoom(room, function(err, res) {
+            var packet = new CodexPacket();
+            if (err) {
+                packet.type = "error";
+                packet.data.cmd = "onjoin";
+                packet.data.value = room;
+                packet.receiver = pkg.sender;
+                packet.Send();
+                return;
+            } else {
+                packet.type = "onjoin"
+                packet.data.message = "Joined Room " + room;
+                packet.receiver = pkg.sender;
+                packet.Send();
 
-                    window.setTimeout(function () {
-                    webrtc.sendToAll('nickname', {nick: nick});
-                        if(gender == 0){
-                            webrtc.sendToAll('avatar', {avatar: "img/female.png"});
-                        }
-                        else{
-                            webrtc.sendToAll('avatar', {avatar: "img/male.png"});
-                        }
+                window.setTimeout(function() {
+                    webrtc.sendToAll('nickname', { nick: nick });
+                    if (gender == 0) {
+                        webrtc.sendToAll('avatar', { avatar: "img/female.png" });
+                    } else {
+                        webrtc.sendToAll('avatar', { avatar: "img/male.png" });
+                    }
 
-                    }, 2000);
-                }
-                
-            });
-        
-    }
-    else if(pkg.type == "leave"){
+                }, 2000);
+            }
+
+        });
+
+    } else if (pkg.type == "leave") {
         webrtc.leaveRoom();
-        var packet = new CodexPacket();
-        packet.type = "onleave"
-        packet.data.message = "Left Room " + room;
-        packet.receiver = pkg.sender;
-        packet.Send();
-    }
-    else if(pkg.type == "disconnect"){
+
+    } else if (pkg.type == "disconnect") {
         webrtc.disconnect();
         var packet = new CodexPacket();
         packet.type = "ondisconnect"
         packet.data.message = "Disconnected";
         packet.receiver = pkg.sender;
         packet.Send();
-    }
-     else if(pkg.type == "stop"){
+    } else if (pkg.type == "stop") {
         webrtc.stopLocalVideo();
         var packet = new CodexPacket();
         packet.type = "onstop"
         packet.data.message = "Stopped";
         packet.receiver = pkg.sender;
         packet.Send();
-    }
-    else if(pkg.type == "restart"){
+    } else if (pkg.type == "restart") {
         webrtc.startLocalVideo();
-    }
-    else if(pkg.type == "avatar"){
+    } else if (pkg.type == "avatar") {
 
         //Get and set Name to peers
         nick = pkg.data.name.replace(/\s/g, '-').replace(/[^A-Za-z0-9_\-]/g, '');
-        webrtc.sendToAll('nickname', {nick: nick});
+        webrtc.sendToAll('nickname', { nick: nick });
         document.getElementById('nickInput').innerHTML = nick;
 
         //Get and set Gender to peers
         gender = pkg.data.gender;
-        if(gender.toLowerCase() == 'male'){
+        if (gender.toLowerCase() == 'male') {
             document.getElementById('localAvatar').src = "img/male.png";
-            webrtc.sendToAll('avatar', {avatar: "img/male.png"});
-        }
-        else{
+            webrtc.sendToAll('avatar', { avatar: "img/male.png" });
+        } else {
             document.getElementById('localAvatar').src = "img/female.png";
-            webrtc.sendToAll('avatar', {avatar: "img/female.png"});
+            webrtc.sendToAll('avatar', { avatar: "img/female.png" });
         }
-    }
-    else{
+    } else {
         cosole.log("Unknown Packet Type: " + e.data);
     }
 }
 
-var Load = function(){
+var Load = function() {
     var packet = new CodexPacket();
     packet.type = "oniframeload"
     packet.data.message = "iframe Loaded";
